3eeebc0a7c0e0e54a75619aa1e7c8f828e53e327
blitz <banacorn@gmail.com>
2012-11-09 03:48:42 +0800

    convert indentation from tabs to spaces

--- automaton/re.hs
+++ automaton/re.hs
@@ -3,9 +3,14 @@ module Automaton.RE where
 data RE = A Char | N | E | Union RE RE | Concat RE RE | Star RE
 
 instance Show RE where
-	show (A a) = [a]
-	show N = "∅"
-	show E = " "
-	show (Union a b) = "(" ++ show a ++ "|" ++ show b ++ ")"
-	show (Concat a b) = show a ++ show b
-	show (Star a) = show a ++ "*"
+    show (A a) = [a]
+    show N = "∅"
+    show E = ""
+    show (Union a b) = "(" ++ show a ++ "|" ++ show b ++ ")"
+    show (Concat a b) = show a ++ show b
+    show (Star a) = show a ++ "*"
+
+--instance Read RE where
+--     readsPrec _ r = do
+--    ("Z", s) <- lex r
+--    return (Z, s)
