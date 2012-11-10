3f9b7c42d717e7490fdd7301cc1e28a2b446f9c7
blitz <banacorn@gmail.com>
2012-11-10 16:32:54 +0800

    RE read & show instances

--- automaton/re.hs
+++ automaton/re.hs
@@ -1,16 +1,74 @@
 module Automaton.RE where
 
-data RE = A Char | N | E | Union RE RE | Concat RE RE | Star RE
+import Debug.Trace
+import Text.ParserCombinators.Parsec
+
+infixr 5 :+
+infixr 4 :|
+data RE = A Char | N | E |  RE :| RE | RE :+ RE | Star RE
+
 
 instance Show RE where
     show (A a) = [a]
     show N = "∅"
-    show E = ""
-    show (Union a b) = "(" ++ show a ++ "|" ++ show b ++ ")"
-    show (Concat a b) = show a ++ show b
+    show E = " "
+    show (a :| b) = "(" ++ show a ++ "|" ++ show b ++ ")"
+    show (a :+ b) = show a ++ show b
     show (Star a) = show a ++ "*"
 
---instance Read RE where
---     readsPrec _ r = do
---    ("Z", s) <- lex r
---    return (Z, s)
+instance Read RE where
+    readsPrec _ input = case parse reParser "Regular Expression" input of
+        Right x -> [(x, "")]
+
+unitParser :: Parser RE
+unitParser =
+    do
+        char '('
+        inside <- reParser
+        char ')'
+        do 
+            char '*'
+            return (Star inside)
+            <|> return (inside)
+    <|> 
+    do
+        char ' '
+        do
+            char '*'
+            return (Star (E))
+            <|> return (E)
+    <|> 
+    do
+        c <- digit
+        do
+            char '*'
+            return (Star (A c))
+            <|> return (A c)
+    <|> 
+    do
+        c <- letter
+        do
+            char '*'
+            return (Star (A c))
+            <|> return (A c)
+
+concatParser :: Parser RE
+concatParser = 
+    do 
+        a <- many1 unitParser
+        return $ foldr1 (:+) a
+
+reParser :: Parser RE
+reParser =
+    do
+        a <- concatParser
+        do
+            char '|'
+            b <- concatParser
+            return (a :| b)
+            <|> return a
+
+
+
+
+
