ec677318142e895556e512a95bea14ead5caa002
blitz <banacorn@gmail.com>
2012-11-22 16:18:59 +0800

    tabs -> spaces

--- automaton/pda.hs
+++ automaton/pda.hs
@@ -5,11 +5,13 @@ import Automaton.Util
 
 import Control.Applicative
 
+
 automatonPDA :: PDA -> Language -> Bool
 
+-- rejects everything if there are no accept states
 automatonPDA (PDA _ _ _ _ _ _ []) _ = False
 automatonPDA (PDA states alphabets stateAlphabets (TransitionsPDA transitions) state stackTop acceptStates) []
-	| state `elem` acceptStates = True
-	| or $ flip elem acceptStates <$> (epsilonClosure (TransitionsPDA transitions) state) = True
-	| otherwise = False
+    | state `elem` acceptStates = True
+    | or $ flip elem acceptStates <$> (epsilonClosure (TransitionsPDA transitions) state) = True
+    | otherwise = False
 
