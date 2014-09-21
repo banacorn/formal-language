dfbccb02dd44a0be21601a2403378bee99d043f3
Cychih <michael66230@gmail.com>
2014-09-22 02:18:33 +0800

    refine wait_for_compile.sh

--- wait_for_compile.sh
+++ wait_for_compile.sh
@@ -13,7 +13,11 @@ do
     echo "Input file: $filepath"
     agda $filepath
     echo "All Goals:"
+    esc=`printf '\033'`
     echo "IOTCM \"$filepath\" NonInteractive Indirect (Cmd_load \"$filepath\" [])" | agda --interaction | grep "All Goals" | sed 's/(agda2-info-action ".All Goals." "\(.*\)" nil)/\1/' | sed 's/\\n/\
-/g' | awk '{ if ($0 != "") {print "  " $0}}'
+/g' | sed "s/?\([0123456789]*\) \(.*\)$/${esc}[1;31m?\1${esc}[m \2/g" | sed "s/→/${esc}[1;33m→${esc}[m/g" | awk '{ if ($0 != "") {print "  " $0}}'
+
+#?0 : y == z → x == z
+#|0 : y == z → x ==|z
 done
 
