acb04988628d0b4f8e9e08ed5a28699d518bef22
Cychih <michael66230@gmail.com>
2014-09-22 01:20:27 +0800

    refine wait_for_compile.sh

--- wait_for_compile.sh
+++ wait_for_compile.sh
@@ -8,7 +8,12 @@ while [ 1 ]
 do
     echo "=============================="
     filename=`nc -l 1531 | sed "s/;//g" | sed "s/ //g"`
+    filepath="`pwd -P`/$filename"
     date
-    echo "Input file: $filename"
-    agda $filename
+    echo "Input file: $filepath"
+    agda $filepath
+    echo "All Goals:"
+    echo "IOTCM \"$filepath\" NonInteractive Indirect (Cmd_load \"$filepath\" [])" | agda --interaction | grep "All Goals" | sed 's/(agda2-info-action ".All Goals." "\(.*\)" nil)/\1/' | sed 's/\\n/\
+/g' | awk '{ if ($0 != "") {print "  " $0}}'
 done
+
